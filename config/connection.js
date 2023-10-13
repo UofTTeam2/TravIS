6488812be7f8772f4c2c1f308b278be56626b845
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-10-13 18:39:49 -0400

    feat(connection): create .env file and connection.js to connect to database

---
+++ config/connection.js
@@ -0,0 +1,23 @@
+const Sequelize = require("sequelize");
+// Enable access to .env variables
+require("dotenv").config();
+
+let sequelize;
+
+// Use JawsDB if deployed on Heroku
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Use environment variables to connect to database
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+module.exports = sequelize;
