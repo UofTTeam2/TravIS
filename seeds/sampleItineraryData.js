92d56c4ca62bbb6f092aa8016d560a5c32570256
ethan <hertlethan@gmail.com>
2023-10-19 16:11:36 -0400

    fix(handlebars): fixed bug where uploading an image would update the first preview element in the category, even if the image was uploaded for the second, third, etc. item, also updated sample itinerary data to better simulate the data the handlebars file will be recieving

--- seeds/sampleItineraryData.js
+++ seeds/sampleItineraryData.js
@@ -1,53 +1,178 @@
-let accommodationItems = {
-    item1: {
-        title: 'test 1',
-        link: 'https://github.com/UofTTeam2/TravIs/tree/dev',
-        image: 'https://i.pinimg.com/736x/b2/32/63/b23263218ede5b69d4e1a7d01660410c.jpg',
-        startDate: '2023-10-17',
-        startTime: '4:00 PM',
-        endDate: '2024-01-19',
-        endTime: '11:00 AM',
-        price: '$400',
-        notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+let section_1 = {
+    section_title: 'tokyo',
+    accommodationItems: {
+        item1: {
+            title: 'test 1',
+            link: 'https://github.com/UofTTeam2/TravIs/tree/dev',
+            image: 'https://i.pinimg.com/736x/b2/32/63/b23263218ede5b69d4e1a7d01660410c.jpg',
+            startDate: '2023-10-17',
+            startTime: '4:00 PM',
+            endDate: '2024-01-19',
+            endTime: '11:00 AM',
+            price: '$400',
+            notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+        },
+        item2: {
+            title: 'test 2',
+            link: 'https://drinkarizona.com/collections/hats/products/big-blossom-bucket-hat',
+            image: 'https://cultureofgaming.com/wp-content/uploads/2023/02/the-way-of-the-kings-stormlight-archives.png',
+            startDate: '2023-10-17',
+            startTime: '4:00 PM',
+            endDate: '2024-01-19',
+            endTime: '11:00 AM',
+            price: '$400',
+            notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+        }
     },
-    item2: {
-        title: 'test 2',
-        link: 'https://drinkarizona.com/collections/hats/products/big-blossom-bucket-hat',
-        image: 'https://cultureofgaming.com/wp-content/uploads/2023/02/the-way-of-the-kings-stormlight-archives.png',
-        startDate: '2023-10-17',
-        startTime: '4:00 PM',
-        endDate: '2024-01-19',
-        endTime: '11:00 AM',
-        price: '$400',
-        notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+    foodItems: {
+        item1: {
+            title: 'peach tea',
+            link: 'https://minimalistbaker.com/perfect-peach-iced-tea/',
+            image: 'https://minimalistbaker.com/wp-content/uploads/2014/06/Perfect-sweet-Peached-Iced-Tea-Perfect-for-summer-and-pool-days..jpg',
+            startDate: '2023-10-17',
+            startTime: '4:00 PM',
+            endDate: '2024-01-19',
+            endTime: '11:00 AM',
+            price: '$400',
+            notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+        },
+        item2: {
+            title: 'the way of kings',
+            link: 'https://www.michaelwhelan.com/shop/way-of-kings/',
+            image: 'https://i.redd.it/pty7jq8xvsh71.jpg',
+            startDate: '2023-10-17',
+            startTime: '4:00 PM',
+            endDate: '2024-01-19',
+            endTime: '11:00 AM',
+            price: '$5000',
+            notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+        },
+        item3: {
+            title: '',
+            link: '',
+            image: '',
+            startDate: '2023-10-17',
+            startTime: '',
+            endDate: '2024-01-19',
+            endTime: '',
+            price: '',
+            notes: '',
+        }
+    },
+    transportItems: {
+        item1: {
+            title: 'the way of kings',
+            link: '',
+            image: 'https://i.redd.it/pty7jq8xvsh71.jpg',
+            startDate: '2023-10-17',
+            startTime: '',
+            endDate: '2024-01-19',
+            endTime: '',
+            price: '',
+            notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+        }},
+    activityItems: {},
+    miscItems: {
+        item1: {
+            title: '',
+            link: 'https://www.michaelwhelan.com/shop/way-of-kings/',
+            image: '',
+            startDate: '2023-10-17',
+            startTime: '4:00 PM',
+            endDate: '',
+            endTime: '11:00 AM',
+            price: '$5000',
+            notes: '',
+        }
     }
 };
-let foodItems = {
-    item1: {
-        title: 'peach tea',
-        link: 'https://minimalistbaker.com/perfect-peach-iced-tea/',
-        image: 'https://minimalistbaker.com/wp-content/uploads/2014/06/Perfect-sweet-Peached-Iced-Tea-Perfect-for-summer-and-pool-days..jpg',
-        startDate: '2023-10-17',
-        startTime: '4:00 PM',
-        endDate: '2024-01-19',
-        endTime: '11:00 AM',
-        price: '$400',
-        notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+let section_2 = {
+    section_title: 'osaka',
+    accommodationItems: {
+        item1: {
+            title: 'test 1',
+            link: 'https://github.com/UofTTeam2/TravIs/tree/dev',
+            image: 'https://i.pinimg.com/736x/b2/32/63/b23263218ede5b69d4e1a7d01660410c.jpg',
+            startDate: '2023-10-17',
+            startTime: '4:00 PM',
+            endDate: '2024-01-19',
+            endTime: '11:00 AM',
+            price: '$400',
+            notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+        },
+        item2: {
+            title: 'test 2',
+            link: 'https://drinkarizona.com/collections/hats/products/big-blossom-bucket-hat',
+            image: 'https://cultureofgaming.com/wp-content/uploads/2023/02/the-way-of-the-kings-stormlight-archives.png',
+            startDate: '2023-10-17',
+            startTime: '4:00 PM',
+            endDate: '2024-01-19',
+            endTime: '11:00 AM',
+            price: '$400',
+            notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+        }
     },
-    item2: {
-        title: 'the way of kings',
-        link: 'https://www.michaelwhelan.com/shop/way-of-kings/',
-        image: 'https://i.redd.it/pty7jq8xvsh71.jpg',
-        startDate: '2023-10-17',
-        startTime: '4:00 PM',
-        endDate: '2024-01-19',
-        endTime: '11:00 AM',
-        price: '$5000',
-        notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+    foodItems: {
+        item1: {
+            title: 'peach tea',
+            link: 'https://minimalistbaker.com/perfect-peach-iced-tea/',
+            image: 'https://minimalistbaker.com/wp-content/uploads/2014/06/Perfect-sweet-Peached-Iced-Tea-Perfect-for-summer-and-pool-days..jpg',
+            startDate: '2023-10-17',
+            startTime: '4:00 PM',
+            endDate: '2024-01-19',
+            endTime: '11:00 AM',
+            price: '$400',
+            notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+        },
+        item2: {
+            title: 'the way of kings',
+            link: 'https://www.michaelwhelan.com/shop/way-of-kings/',
+            image: 'https://i.redd.it/pty7jq8xvsh71.jpg',
+            startDate: '2023-10-17',
+            startTime: '4:00 PM',
+            endDate: '2024-01-19',
+            endTime: '11:00 AM',
+            price: '$5000',
+            notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+        },
+        item3: {
+            title: '',
+            link: '',
+            image: '',
+            startDate: '2023-10-17',
+            startTime: '',
+            endDate: '2024-01-19',
+            endTime: '',
+            price: '',
+            notes: '',
+        }
+    },
+    transportItems: {
+        item1: {
+            title: 'the way of kings',
+            link: '',
+            image: 'https://i.redd.it/pty7jq8xvsh71.jpg',
+            startDate: '2023-10-17',
+            startTime: '',
+            endDate: '2024-01-19',
+            endTime: '',
+            price: '',
+            notes: 'AS a frequent traveler, WHEN I travel in VR, I WANT to see Ehsan there as well',
+        }},
+    activityItems: {},
+    miscItems: {
+        item1: {
+            title: '',
+            link: 'https://www.michaelwhelan.com/shop/way-of-kings/',
+            image: '',
+            startDate: '2023-10-17',
+            startTime: '4:00 PM',
+            endDate: '',
+            endTime: '11:00 AM',
+            price: '$5000',
+            notes: '',
+        }
     }
 };
-let transportItems;
-let activityItems;
-let miscItems;
 
-module.exports = {accommodationItems, foodItems, transportItems, activityItems, miscItems};
+module.exports = {section_1, section_2};
