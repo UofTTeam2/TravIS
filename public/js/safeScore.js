f7ccd0138436743e7a7f7cd2add9d71f51ac5054
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-19 23:30:13 -0400

    update/docs:(safeScore.js) updated comments, updated error handling message

--- public/js/safeScore.js
+++ public/js/safeScore.js
@@ -1,22 +1,29 @@
-const Amadeus = require('amadeus');
-const fetchCityData = require('./geoCode');
-require('dotenv').config();
+//Description: fetches safety scores for a city out of 100
+// =============================================================
 
+// Dependencies
+// =============================================================
+const Amadeus = require('amadeus');
+const fetchCityData = require('./geoCode'); //gets coordinates of user's location from geoCode.js
+require('dotenv').config();
+// =============================================================
+
+// Amadeus API call
+// =============================================================
 const amadeus = new Amadeus({
     clientId: 'VQKv0PtyKl6XGnJyaYaHS8UPD9fCeCmb',
     clientSecret: 'TWb8Y2pxxA1Np1UZ',
     // clientId: process.env.AMADEUS_CLIENT_ID,
     // clientSecret: process.env.AMADEUS_CLIENT_SECRET,
-
 });
-//get tours and activities
+//get safety scores for user's location, after getting latitude and longitude of user's location first
 async function safeScore() {
     try {
         const { lat, lon } = await fetchCityData();
         if (lat && lon) {
             const response = await amadeus.safety.safetyRatedLocations.get({
                 latitude: lat,
-                longitude: lon
+                longitude: lon,
             });
             const data = response.result;
             const safeData = data.data;
@@ -29,7 +36,8 @@ async function safeScore() {
                 const medical = safeScore.safetyScores.medical;
                 const safeOverall = safeScore.safetyScores.overall;
                 const physicalHarm = safeScore.safetyScores.physicalHarm;
-                const politicalFreedom = safeScore.safetyScores.politicalFreedom;
+                const politicalFreedom =
+                    safeScore.safetyScores.politicalFreedom;
                 const theft = safeScore.safetyScores.theft;
                 const women = safeScore.safetyScores.women;
                 console.log(`Safety Scores 0-100 for ${areaName}, ${areaType}:
@@ -44,11 +52,14 @@ async function safeScore() {
         } else {
             console.log('Error: no latitude/longitude data');
         }
-        // response.render()
+        // response.render('test', { safeData }); //render safeData to test.handlebars
     } catch (error) {
-        console.log(error);
+        console.log('Error', error.message);
     }
 }
+// =============================================================
+// invoke function to get safety scores
 safeScore();
-
+// =============================================================
+// export safeScore function
 module.exports = safeScore;
