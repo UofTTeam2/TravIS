d9beff6f92a5fd59c1a97cace9395d08a34aa593
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-19 21:39:57 -0400

    deleted test route files and assosiated script from api folder and index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,21 +4,20 @@
 // Dependencies:
 //==============================================================
 const router = require('express').Router();
-// const userRoutes = require('./user-routes');
+const userRoutes = require('./user-routes');
+const tripRoutes = require('./trip-routes');
+const locationRoutes = require('./location-routes');
+const commentRoutes = require('./comment-routes');
 const poi = require('./poi');
-const test = require('./test');
-// const tripRoutes = require('./trip-routes');
-// const locationRoutes = require('./location-routes');
-// const commentRoutes = require('./comment-routes');
 
 // Routes:
 //==============================================================
-// router.use('/users', userRoutes);
-// router.use('/trips', tripRoutes);
-// router.use('/locations', locationRoutes);
-// router.use('/comments', commentRoutes);
+router.use('/users', userRoutes);
+router.use('/trips', tripRoutes);
+router.use('/locations', locationRoutes);
+router.use('/comments', commentRoutes);
 router.use('/poi', poi);
-router.use('/test', test);
+
 //==============================================================
 
 module.exports = router;
