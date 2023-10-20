d89bd059b3e0cb9aed2266b14e65543c47543fa4
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-19 23:21:13 -0400

    bug/docs:(toDo.js) fixed undefined return value for price if no price exists, now will display as N/A instead, added comments

--- public/js/toDo.js
+++ public/js/toDo.js
@@ -1,17 +1,27 @@
-const Amadeus = require('amadeus');
-const fetchCityData = require('./geoCode');
-require('dotenv').config();
+//Desc: this file gets the coordinates of the user's location and then uses those coordinates
+//to get a list of recommended tours and activities from the Amadeus API.
+// =============================================================
 
+// Dependencies
+// =============================================================
+const Amadeus = require('amadeus');
+const fetchCityData = require('./geoCode'); //gets coordinates of user's location from geoCode.js
+require('dotenv').config();
+// =============================================================
+
+// Amadeus API call
+// =============================================================
 const amadeus = new Amadeus({
-    clientId: 'VQKv0PtyKl6XGnJyaYaHS8UPD9fCeCmb',
-    clientSecret: 'TWb8Y2pxxA1Np1UZ',
+    clientId: 'VQKv0PtyKl6XGnJyaYaHS8UPD9fCeCmb', //delete for production
+    clientSecret: 'TWb8Y2pxxA1Np1UZ', //delete for production
     // clientId: process.env.AMADEUS_CLIENT_ID,
     // clientSecret: process.env.AMADEUS_CLIENT_SECRET,
 
 });
-//get tours and activities
+//get tours and activities for user's location
 async function thingsToDo() {
     try {
+        //get latitude and longitude of user's location first before making API call
         const { lat, lon } = await fetchCityData();
         if (lat && lon) {
             const response = await amadeus.shopping.activities.get({
@@ -20,18 +30,28 @@ async function thingsToDo() {
             });
             const data = response.result;
             const toDoData = data.data;
-            console.log(toDoData);
+            // console.log(toDoData);
 
             toDoData.forEach((toDo) => {
                 const toDoName = toDo.name;
                 const toDoDescription = toDo.description;
-                const toDoPrice = toDo.price;
+                //set default values for price and currency, in case they are not available
+                let toDoPrice = 'N/A';
+                let toDoCurrency = 'N/A';
+
+                // Check if price object and its properties exist, then assign values
+                if (toDo.price && toDo.price.amount && toDo.price.currencyCode) {
+                    toDoPrice = toDo.price.amount;
+                    toDoCurrency = toDo.price.currencyCode;
+                }
+
                 const toDoPics = toDo.pictures;
                 const toDoBooking = toDo.bookingLink;
                 const toDoDuration = toDo.minimumDuration;
-                console.log(`name: ${toDoName}, 
+                console.log(`Here are some recommended activities for your location:
+                name: ${toDoName}, 
                 description: ${toDoDescription},
-                price: ${toDoPrice}, 
+                price: ${toDoPrice} ${toDoCurrency}, 
                 pictures: ${toDoPics}, 
                 booking_link: ${toDoBooking},
                 minimum_duration: ${toDoDuration}`);
@@ -39,12 +59,14 @@ async function thingsToDo() {
         } else {
             console.log('Error: no latitude/longitude data');
         }
-        // response.render()
+        // response.render('test', { toDoData }); //render toDoData to test.handlebars
     } catch (error) {
-        console.log(error);
+        console.log('Error:', error.message);
     }
 }
-
+// =============================================================
+// invoke function to get tours and activities
 thingsToDo();
-
+// =============================================================
+// export thingsToDo function
 module.exports = thingsToDo;
