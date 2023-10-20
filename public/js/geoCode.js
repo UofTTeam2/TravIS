b1d787389784042ea653e05d8b27e2288d167e08
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-19 23:52:49 -0400

    update/docs:(geoCode.js) updated/added comments, commented out console logs, updated catch error handlers

--- public/js/geoCode.js
+++ public/js/geoCode.js
@@ -1,14 +1,22 @@
-require('dotenv').config();
+//Description: fetches latitude and longitude of a city based on user input,
+// to be used in other API calls
+// =============================================================
 
-//https://api-ninjas.com/api/geocoding
+// Dependencies
+require('dotenv').config();
+// =============================================================
+
+//https://api-ninjas.com/api/geocoding - API documentation
 // city defined for testing purposes
 const city = 'Berlin';
-// use this line for production, change element id to match input field
+// use next 2 lines for production, change element id to match input field
 // const city = document.getElementById('cityInput').value;
 // const apiKey = process.env.GEOCODE_API_KEY;
-const apiKey = 'ZqSCB/0BSxQD4+VQ8BG+cA==Exh8YYVi9hdMvbfb';
+const apiKey = 'ZqSCB/0BSxQD4+VQ8BG+cA==Exh8YYVi9hdMvbfb';//comment out for production
 const apiUrl = `https://api.api-ninjas.com/v1/geocoding?city=${city}`;
 
+//API call to get latitude and longitude of city
+// =============================================================
 async function fetchCityData() {
     try {
         const response = await fetch(apiUrl, {
@@ -24,16 +32,17 @@ async function fetchCityData() {
             // console.log(result);
             const lat = result[0].latitude;
             const lon = result[0].longitude;
-            console.log('Latitude:', lat, 'Longitude:', lon);
+            // console.log('Latitude:', lat, 'Longitude:', lon);
             return { lat, lon};
         } else {
             console.error('Error:', response.statusText);
             return null;
         }
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error:', error.message);
         return null;
     }
 }
-
+// =============================================================
+//export function to be used in other files
 module.exports = fetchCityData;
