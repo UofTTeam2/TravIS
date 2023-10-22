338800211cffe88e5b2786c375e41b9b597df5e2
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-22 00:43:25 -0400

    trying to simplify route code where everything is on one route, kept getting 400 error, went back to old way with multiple paths, getting 200 response but nothing rendering to page. Will have to check js files again to see what is wrong.

--- controllers/api/poi.js
+++ controllers/api/poi.js
@@ -9,7 +9,7 @@ const router = require('express').Router();
 const Amadeus = require('amadeus');
 require('dotenv').config();
 
-//gets longitude and latitude of user's location, imported from geoCode.js
+// gets longitude and latitude of user's location, imported from geoCode.js
 const fetchCityData = require('../../public/js/geoCode');
 const getPOI = require('../../public/js/poi');
 const thingsToDo = require('../../public/js/toDo');
@@ -29,12 +29,144 @@ const amadeus = new Amadeus({
 // Get points of interest
 //======================================================================
 router.get('/', async (req, res) => {
+//     try{
+//         const poiData = await getPOI();
+//         const toDoData = await thingsToDo();
+//         const safeData = await safeScore();
+//         const locationData = await locationScore();
+
+    //         if (poiData && toDoData && safeData && locationData) {
+    //             res.render('poi', { poiData, toDoData, safeData, locationData});
+    //         } else {
+    //             console.log('Error: no data received');
+    //             res.status(400).json({ error: 'No data received' });
+    //         }
+    //     } catch (err) {
+    //         console.error(err);
+    //         res.status(500).json({ error: 'Internal server error' });
+    //     }
+    // });
+
+    try{
+        //get latitude and longitude of user's location first before making API call
+        const { lat, lon } = await fetchCityData();
+        if (lat && lon) {
+            //get points of interest from Amadeus API
+            const response = await amadeus.referenceData.locations.pointsOfInterest.get({
+                latitude: lat,
+                longitude: lon
+            });
+            const data = response.result; //the fully parsed data
+            //define poiData as the data from the API call (array of objects)
+            const poiData = data.data;
+            // console.log(poiData);
+            res.status(200).render('poi', { poiData}); //render to page using handlebars
+            // res.render('poi', { poiData: poiData }); //render to page using handlebars
+        } else {
+            console.log('Error: no latitude/longitude data');
+            res.status(400).json({ error: 'No latitude/longitude data' });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+//Get tours and activities
+//======================================================================
+router.get('/activities', async (req, res) => {
+
+    try{
+        //get latitude and longitude of user's location first before making API call
+        const { lat, lon } = await fetchCityData();
+        if (lat && lon) {
+
+            const response = await amadeus.shopping.activities.get({
+                latitude: lat,
+                longitude: lon
+            });
+            const data = response.result; //the fully parsed data
+            //define toDoData as the data from the API call (array of objects)
+            const toDoData = data.data;
+            // console.log(toDoData);
+            res.status(200).render('poi', { toDoData }); //render to page using handlebars
+            // res.render('poi', { toDoData: toDOData }); //render to page using handlebars
+        } else {
+            console.log('Error: no latitude/longitude data');
+            res.status(400).json({ error: 'No latitude/longitude data' });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+//Get safety score
+//======================================================================
+router.get('/safety', async (req, res) => {
+
+    try{
+        //get latitude and longitude of user's location first before making API call
+        const { lat, lon } = await fetchCityData();
+        if (lat && lon) {
+
+            const response = await amadeus.safety.safetyRatedLocations.get({
+                latitude: lat,
+                longitude: lon
+            });
+            const data = response.result; //the fully parsed data
+            //define safeData as the data from the API call (array of objects)
+            const safeData = data.data;
+            // console.log(safeData);
+            res.status(200).render('poi', { safeData }); //render to page using handlebars
+            // res.render('poi', { safeData: safeData }); //render to page using handlebars
+        } else {
+            console.log('Error: no latitude/longitude data');
+            res.status(400).json({ error: 'No latitude/longitude data' });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+//Get location score
+//======================================================================
+router.get('/location', async (req, res) => {
+
+    try{
+        //get latitude and longitude of user's location first before making API call
+        const { lat, lon } = await fetchCityData();
+        if (lat && lon) {
+
+            const response = await amadeus.location.analytics.categoryRatedAreas.get({
+                latitude: lat,
+                longitude: lon
+            });
+            const data = response.result; //the fully parsed data
+            //define locationData as the data from the API call (array of objects)
+            const locationData = data.data;
+            // console.log(locationData);
+            res.status(200).render('poi', { locationData }); //send data to client
+            // res.render('poi', { locationData: locagtionData }); //render to page using handlebars
+        } else {
+            console.log('Error: no latitude/longitude data');
+            res.status(400).json({ error: 'No latitude/longitude data' });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// if/when all data is received, render to page using handlebars
+//======================================================================
+router.get('/', async (req, res) => {
     try{
         const poiData = await getPOI();
         const toDoData = await thingsToDo();
         const safeData = await safeScore();
         const locationData = await locationScore();
-
         if (poiData && toDoData && safeData && locationData) {
             res.render('poi', { poiData, toDoData, safeData, locationData, city });
         } else {
@@ -47,138 +179,4 @@ router.get('/', async (req, res) => {
     }
 });
 
-//     try{
-//         //get latitude and longitude of user's location first before making API call
-//         const { lat, lon } = await fetchCityData();
-//         if (lat && lon) {
-//             //get points of interest from Amadeus API
-//             const response = await amadeus.referenceData.locations.pointsOfInterest.get({
-//                 latitude: lat,
-//                 longitude: lon
-//             });
-//             const data = response.result; //the fully parsed data
-//             //define poiData as the data from the API call (array of objects)
-//             const poiData = data.data;
-//             // console.log(poiData);
-//             res.status(200).render('poi', { poiData}); //render to page using handlebars
-//             // res.render('poi', { poiData: poiData }); //render to page using handlebars
-//         } else {
-//             console.log('Error: no latitude/longitude data');
-//             res.status(400).json({ error: 'No latitude/longitude data' });
-//         }
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
-
-// //Get tours and activities
-// //======================================================================
-// router.get('/activities', async (req, res) => {
-
-//     try{
-//         //get latitude and longitude of user's location first before making API call
-//         const { lat, lon } = await fetchCityData();
-//         if (lat && lon) {
-
-//             const response = await amadeus.shopping.activities.get({
-//                 latitude: lat,
-//                 longitude: lon
-//             });
-//             const data = response.result; //the fully parsed data
-//             //define toDoData as the data from the API call (array of objects)
-//             const toDoData = data.data;
-//             // console.log(toDoData);
-//             res.status(200).render('poi', { toDoData }); //render to page using handlebars
-//             // res.render('poi', { toDoData: toDOData }); //render to page using handlebars
-//         } else {
-//             console.log('Error: no latitude/longitude data');
-//             res.status(400).json({ error: 'No latitude/longitude data' });
-//         }
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
-
-// //Get safety score
-// //======================================================================
-// router.get('/safety', async (req, res) => {
-
-//     try{
-//         //get latitude and longitude of user's location first before making API call
-//         const { lat, lon } = await fetchCityData();
-//         if (lat && lon) {
-
-//             const response = await amadeus.safety.safetyRatedLocations.get({
-//                 latitude: lat,
-//                 longitude: lon
-//             });
-//             const data = response.result; //the fully parsed data
-//             //define safeData as the data from the API call (array of objects)
-//             const safeData = data.data;
-//             // console.log(safeData);
-//             res.status(200).render('poi', { safeData }); //render to page using handlebars
-//             // res.render('poi', { safeData: safeData }); //render to page using handlebars
-//         } else {
-//             console.log('Error: no latitude/longitude data');
-//             res.status(400).json({ error: 'No latitude/longitude data' });
-//         }
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
-
-// //Get location score
-// //======================================================================
-// router.get('/location', async (req, res) => {
-
-//     try{
-//         //get latitude and longitude of user's location first before making API call
-//         const { lat, lon } = await fetchCityData();
-//         if (lat && lon) {
-
-//             const response = await amadeus.location.analytics.categoryRatedAreas.get({
-//                 latitude: lat,
-//                 longitude: lon
-//             });
-//             const data = response.result; //the fully parsed data
-//             //define locationData as the data from the API call (array of objects)
-//             const locationData = data.data;
-//             // console.log(locationData);
-//             res.status(200).render('poi', { locationData }); //send data to client
-//             // res.render('poi', { locationData: locagtionData }); //render to page using handlebars
-//         } else {
-//             console.log('Error: no latitude/longitude data');
-//             res.status(400).json({ error: 'No latitude/longitude data' });
-//         }
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
-
-// // if/when all data is received, render to page using handlebars
-// //======================================================================
-// router.get('/', async (req, res) => {
-//     try{
-//         const poiData = await getPOI();
-//         const toDoData = await thingsToDo();
-//         const safeData = await safeScore();
-//         const locationData = await locationScore();
-//         if (poiData && toDoData && safeData && locationData) {
-//             res.render('poi', { poiData, toDoData, safeData, locationData, city });
-//         } else {
-//             console.log('Error: no data received');
-//             res.status(400).json({ error: 'No data received' });
-//         }
-//     } catch (err) {
-//         console.error(err);
-//         res.status(500).json({ error: 'Internal server error' });
-//     }
-// });
-
 module.exports = router;
-
-// res.render('poi', { poiData, toDoData, safeData, locationData });
--- public/js/poiIndex.js
+++ public/js/poiIndex.js
@@ -1,30 +1,35 @@
 require('dotenv').config();
+const Handlebars = require('handlebars');
 // const Amadeus = require('amadeus');
 // const fetchCityData = require('./geoCode');
 const locationScore = require('./locationScore');
 const getPOI = require('./poi');
 const safeScore = require('./safeScore');
 const thingsToDo = require('./toDo');
-// const template = require('../../views/poi.handlebars');
-const city = document.getElementById('cityInput').value;
+const template = require('../../views/poi.handlebars');
+const compiledTemplate = Handlebars.compile(template);
+
+
+// const city = document.getElementById('cityInput').value;
+const city = 'Barcelona';
 
 document.getElementById('searchForm').addEventListener('submit', async function(event) {
     event.preventDefault();
-    // const city = document.getElementById('cityInput').value;
-    // console.log(city);
-    const poiData = await getPOI(city);
-    const toDoData = await thingsToDo(city);
-    const safeData = await safeScore(city);
-    const locationData = await locationScore(city);
+    const city = document.getElementById('cityInput').value;
+    console.log(city);
+    try {
+        const poiData = await getPOI();
+        const toDoData = await thingsToDo();
+        const safeData = await safeScore();
+        const locationData = await locationScore();
 
-    console.log(poiData);
-    console.log(toDoData);
-    console.log(safeData);
-    console.log(locationData);
+        const renderHTML = compiledTemplate({ poiData, toDoData, safeData, locationData, city });
+        document.querySelector('#recommendations').innerHTML = renderHTML;
 
-    document.querySelector('.hide').classList.remove('hide');
-
-    // const renderHTML = template({ poiData, toDoData, safeData, locationData });
-    // document.querySelector('recommendations').innerHTML = renderHTML;
+        document.querySelector('.hide').classList.remove('hide');
+    } catch (error) {
+        console.error(error);
+    }
 });
+
 module.exports = city;
