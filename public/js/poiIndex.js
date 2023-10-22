7e7fb0d5b82dd33b08ee1acfe6e09b940c11df70
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-22 16:39:43 -0400

    update: (poiIndex.js) started restructuring api call based on new router code

--- public/js/poiIndex.js
+++ public/js/poiIndex.js
@@ -1,31 +1,39 @@
 require('dotenv').config();
 const Handlebars = require('handlebars');
-// const Amadeus = require('amadeus');
-// const fetchCityData = require('./geoCode');
+const fetchCityData = require('./geoCode');
+const Amadeus = require('amadeus');
 const locationScore = require('./locationScore');
 const getPOI = require('./poi');
 const safeScore = require('./safeScore');
 const thingsToDo = require('./toDo');
-const template = require('../../views/poi.handlebars');
-const compiledTemplate = Handlebars.compile(template);
+const template = require('../../views/poi.handlebars'); // Handlebars template
+const compiledTemplate = Handlebars.compile(template); // compiles template
 
-
-const city = document.getElementById('cityInput').value;
-// const city = 'Berlin';
-
+// event listener for city search button
 document.getElementById('searchForm').addEventListener('submit', async function(event) {
     event.preventDefault();
+    // get city name from user input to be used elsewhere
     const city = document.getElementById('cityInput').value;
     console.log(city);
-    try {
-        const poiData = await getPOI();
-        const toDoData = await thingsToDo();
-        const safeData = await safeScore();
-        const locationData = await locationScore();
 
+    try {
+        const { lat, lon } = await fetchCityData(city);
+
+        // api calls to get data
+        // const [poiData, toDoData, safeData, locationData] = await Promise.all([
+
+        // functions here to get data
+        
+    // ]);
+
+
+        //compile template with received data
         const renderHTML = compiledTemplate({ poiData, toDoData, safeData, locationData, city });
+        
+        //update HTML with compiled template
         document.querySelector('#recommendations').innerHTML = renderHTML;
 
+        // show hidden elements with data
         document.querySelector('.hide').classList.remove('hide');
     } catch (error) {
         console.error(error);
