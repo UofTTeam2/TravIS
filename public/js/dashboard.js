75ba292788565a9f53e508aa11760ee0e012b8d3
Carlos Macias <cimm21187@gmail.com>
2023-10-23 17:00:16 -0400

    chore(dashboard):fixed minor issues with style.css

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,31 +21,68 @@ function closeSidebar() {
 }
 
 // Append Card
+
 function appendCard() {
-    const mainCards = document.querySelector('.main-cards');
-    const plusCard = document.querySelector('.main-cards .card:last-child');
-  
-    const newCard = document.createElement('div');
-    newCard.className = 'card';
-  
-    const cardInner = document.createElement('div');
-    cardInner.className = 'card-inner';
-  
-    const pText = document.createElement('p');
-    pText.className = 'text-primary font-weight-bold';
-    pText.textContent = 'New Destination';
-  
-    const pDate = document.createElement('p');
-    pDate.className = 'text-secondary';
-    pDate.textContent = 'Date Here';
-  
-    cardInner.appendChild(pText);
-    newCard.appendChild(cardInner);
-    newCard.appendChild(pDate);
-  
-    // Insert the new card right before the "+" card
-    mainCards.insertBefore(newCard, plusCard);
-  }
-  
-  document.querySelector('.main-cards .card:last-child').addEventListener('click', appendCard);
-  
+  const mainCards = document.querySelector('.main-cards');
+  const plusCard = document.querySelector('.main-cards .add-card');
+
+  // Creating the <a> container
+  const newLink = document.createElement('a');
+  newLink.href = "/trips/edit";
+  newLink.className = 'card-link';
+
+  // Creating the card
+  const newCard = document.createElement('div');
+  newCard.className = 'card';
+
+  const cardInner = document.createElement('div');
+  cardInner.className = 'card-inner';
+
+  const pText = document.createElement('p');
+  pText.className = 'text-primary font-weight-bold';
+  pText.textContent = 'New Destination';
+
+  const pDate = document.createElement('p');
+  pDate.className = 'text-secondary';
+  pDate.textContent = 'Date Here';
+
+  cardInner.appendChild(pText);
+  newCard.appendChild(cardInner);
+  newCard.appendChild(pDate);
+
+  newLink.appendChild(newCard);
+
+  mainCards.insertBefore(newLink, plusCard);
+
+  // Data for the new card
+  const tripData = {
+      title: 'New Destination',
+      date: 'Date Here'
+  };
+
+  fetch('/trips/create-trip', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(tripData),
+  })
+  .then(response => {
+      // Check if the response is successful
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return response.json();
+  })
+  .then(data => {
+      if (data.success) {
+          // Trip successfully created in the database
+          console.log("Trip saved in the database!");
+      } else {
+          console.error("Error saving the trip:", data.error);
+      }
+  })
+  .catch((error) => {
+      console.error('There was a problem with the fetch operation:', error.message);
+  });
+}
