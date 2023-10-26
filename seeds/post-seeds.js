bc8c189bd72c872791387206e3162b5e5ed6b2e7
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-10-26 00:17:06 -0400

    fix(commentSeed): remove a property from comment seed to fix the error

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,5 +1,6 @@
 // Desc: This file will be used to seed the Post table in the database.
 // Author: Cristiano Barboza Godinho
+// A large part of the research for this file was done through the websites W3 Schools (<https://www.w3schools.com/>), MDN Web Docs (<https://developer.mozilla.org/en-US/docs/Web>) and ChatGPT (<https://chat.openai.com/>), and as such small sections of code were adapted from examples offered in those sites.
 // =============================================================
 
 // Import the Post model
