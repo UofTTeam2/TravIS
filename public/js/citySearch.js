2e0f6b324fe41993fcfa8a6cd14add050e6897ff
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-26 00:10:00 -0400

    updated citySearch.js to code from feature/pointsofinterest

--- public/js/citySearch.js
+++ public/js/citySearch.js
@@ -1,37 +1,20 @@
-// require('dotenv').config();
-
-// Function to fetch the coordinates of a city
 const fetchCityData = async (city) => {
-    // const apiKey = process.env.GEOCODE_API_KEY;
-    const apiKey = 'ZqSCB/0BSxQD4+VQ8BG+cA==Exh8YYVi9hdMvbfb';
-    const apiUrl = `https://api.api-ninjas.com/v1/geocoding?city=${city}`;
     try {
-        const response = await fetch(apiUrl, {
-            method: 'GET',
-            headers: {
-                'X-Api-Key': apiKey,
-                'Content-Type': 'application/json',
-            },
-        });
-
+        const response = await fetch(`/api/city?city=${city}`);
         if (response.ok) {
             const result = await response.json();
-            const lat = result[0].latitude;
-            const lon = result[0].longitude;
-            // console.log('The Coordinates of', city, 'are:','Latitude:', lat, 'Longitude:', lon);
+            const lat = result.lat;
+            const lon = result.lon;
             return { lat, lon };
         } else {
-            //handle api errors
             console.error('Error:', response.statusText);
             return null;
         }
     } catch (error) {
-        //handle network or parsing errors
         console.error('Error:', error.message);
         return null;
     }
 };
-
 const getCityData = async (event) => {
     event.preventDefault();
     try {
